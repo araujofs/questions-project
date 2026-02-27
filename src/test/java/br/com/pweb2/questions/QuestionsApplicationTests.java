2a3d583dc1cf8bae5333931a2a0faa331b8dd8a8
Arthur <arthurfelipe5567@gmail.com>
2026-02-27 20:47:07 -0300

    chore: initial commit

---
+++ src/test/java/br/com/pweb2/questions/QuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pweb2.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
