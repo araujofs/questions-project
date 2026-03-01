6a47f15b080cc5f524f5b42d207db4d1e51b9105
Arthur <arthurfelipe5567@gmail.com>
2026-03-01 02:07:47 -0300

    fix: change relation to bidirectional

--- src/main/java/br/com/pweb2/questions/question/Answer.java
+++ src/main/java/br/com/pweb2/questions/question/Answer.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -13,11 +15,37 @@ public class Answer {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  private Long id;
 
   @Column
   private String content;
 
   @Column
   private boolean correct;
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "question_id", nullable = false)
+  private Question question;
+
+  public void setQuestion(Question q) {
+    if (q == null) {
+      throw new IllegalArgumentException("Question argument must not be null");
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (!(o instanceof Answer))
+      return false;
+
+    Answer a = (Answer) o;
+    return id != null && id.equals(a.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }
--- src/main/java/br/com/pweb2/questions/question/Question.java
+++ src/main/java/br/com/pweb2/questions/question/Question.java
@@ -1,12 +1,19 @@
 package br.com.pweb2.questions.question;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import br.com.pweb2.questions.race.Race;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.NoArgsConstructor;
 
@@ -24,6 +31,37 @@ public class Question {
   @Column
   private int points;
 
-  @OneToMany(mappedBy = "question_id")
-  private List<Answer> answers;
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "race_id", nullable = false)
+  private Race race;
+
+  @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = {
+      CascadeType.REMOVE,
+      CascadeType.PERSIST
+  }, orphanRemoval = true)
+  private Set<Answer> answers = new HashSet<>();
+
+  public void setRace(Race r) {
+    if (r == null) {
+      throw new IllegalArgumentException("Race argument must not be null");
+    }
+  }
+
+  public void addAnswer(Answer a) {
+    Objects.requireNonNull(a, "Answer argument must not be null");
+
+    a.setQuestion(this);
+    answers.add(a);
+  }
+
+  public void removeAnswer(Answer a) {
+    Objects.requireNonNull(a, "Answer argument must not be null");
+
+    if (!answers.contains(a)) {
+      throw new IllegalArgumentException("Answer does not belong to Question");
+    }
+
+    a.setQuestion(null);
+    answers.remove(a);
+  }
 }
