b2647a28a20d0e5cbad73797766acc58fa8eebdc
Arthur <arthurfelipe5567@gmail.com>
2026-03-01 02:07:47 -0300

    fix: change relation to bidirectional

--- src/main/java/br/com/pweb2/questions/race/Race.java
+++ src/main/java/br/com/pweb2/questions/race/Race.java
@@ -1,10 +1,14 @@
 package br.com.pweb2.questions.race;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import br.com.pweb2.questions.question.Question;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,6 +32,27 @@ public class Race {
   @Column
   private int duration;
 
-  @OneToMany(mappedBy = "race_id")
-  private List<Question> questions;
+  @OneToMany(mappedBy = "race", fetch = FetchType.LAZY, cascade = {
+      CascadeType.REMOVE,
+      CascadeType.PERSIST
+  }, orphanRemoval = true)
+  private Set<Question> questions = new HashSet<>();
+
+  public void addQuestion(Question q) {
+    Objects.requireNonNull(q, "Question argument must not be null");
+
+    q.setRace(this);
+    questions.add(q);
+  }
+
+  public void removeQuestion(Question q) {
+    Objects.requireNonNull(q, "Question argument must not be null");
+
+    if (!questions.contains(q)) {
+      throw new IllegalArgumentException("Question does not belong to Race");
+    }
+
+    q.setRace(null);
+    questions.remove(q);
+  }
 }
