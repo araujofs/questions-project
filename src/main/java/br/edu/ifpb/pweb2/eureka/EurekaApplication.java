9c5595b4d6f094b296639acf1917db4046b2156c
Arthur <arthurfelipe5567@gmail.com>
2026-03-24 16:12:47 -0300

    feat: add default admin user creation

--- src/main/java/br/edu/ifpb/pweb2/eureka/EurekaApplication.java
+++ src/main/java/br/edu/ifpb/pweb2/eureka/EurekaApplication.java
@@ -1,13 +1,34 @@
 package br.edu.ifpb.pweb2.eureka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.edu.ifpb.pweb2.eureka.user.User;
+import br.edu.ifpb.pweb2.eureka.user.UserRepository;
 
 @SpringBootApplication
 public class EurekaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EurekaApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner initAdminUser(UserRepository userRepo) {
+    var name = "nilce";
+
+    return args -> {
+      if (!userRepo.existsByName(name)) {
+        var admin = new User();
+
+        admin.setAdmin(true);
+        admin.setName(name);
+
+        userRepo.save(admin);
+      }
+    };
   }
 
 }
