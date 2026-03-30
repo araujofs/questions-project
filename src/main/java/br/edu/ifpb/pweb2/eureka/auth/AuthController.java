6e535065381bc8a4a70a12883c6acd9d8eb93871
Arthur <arthurfelipe5567@gmail.com>
2026-03-30 16:01:13 -0300

    feat: move signin handling to spring

--- src/main/java/br/edu/ifpb/pweb2/eureka/auth/AuthController.java
+++ src/main/java/br/edu/ifpb/pweb2/eureka/auth/AuthController.java
@@ -1,17 +1,17 @@
 package br.edu.ifpb.pweb2.eureka.auth;
 
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolderStrategy;
-import org.springframework.security.web.context.SecurityContextRepository;
+// import org.springframework.security.core.context.SecurityContext;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.core.context.SecurityContextHolderStrategy;
+// import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -19,10 +19,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-  private final AuthService service;
-  private final SecurityContextRepository securityContextRepo;
-  private final SecurityContextHolderStrategy securityContextHolderStrat = SecurityContextHolder
-      .getContextHolderStrategy();
+  // private final AuthService service;
+  // private final SecurityContextRepository securityContextRepo;
+  // private final SecurityContextHolderStrategy securityContextHolderStrat =
+  // SecurityContextHolder
+  // .getContextHolderStrategy();
 
   @GetMapping
   public String getLoginPage(Model model) {
@@ -31,23 +32,24 @@ public class AuthController {
     return "signin";
   }
 
-  @PostMapping
-  public String postLoginForm(AuthRequest authBody, HttpServletRequest request, HttpServletResponse response,
-      Model model) {
-    try {
-      var auth = this.service.authenticate(authBody);
-
-      SecurityContext context = securityContextHolderStrat.createEmptyContext();
-      context.setAuthentication(auth);
-      securityContextHolderStrat.setContext(context);
-      securityContextRepo.saveContext(context, request, response);
-
-      return "home";
-    } catch (Exception e) {
-      model.addAttribute("errorMessage", e.getMessage());
-      model.addAttribute("error", true);
-    }
-
-    return "signin";
-  }
+  // @PostMapping
+  // public String postLoginForm(AuthRequest authBody, HttpServletRequest request,
+  // HttpServletResponse response,
+  // Model model) {
+  // try {
+  // var auth = this.service.authenticate(authBody);
+  //
+  // SecurityContext context = securityContextHolderStrat.createEmptyContext();
+  // context.setAuthentication(auth);
+  // securityContextHolderStrat.setContext(context);
+  // securityContextRepo.saveContext(context, request, response);
+  //
+  // return "home";
+  // } catch (Exception e) {
+  // model.addAttribute("errorMessage", e.getMessage());
+  // model.addAttribute("error", true);
+  // }
+  //
+  // return "signin";
+  // }
 }
