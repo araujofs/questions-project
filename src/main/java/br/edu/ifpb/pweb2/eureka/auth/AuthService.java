34315260f7224739cd25641482d4223de96d9a7f
Arthur <arthurfelipe5567@gmail.com>
2026-04-04 20:45:56 -0300

    build: remove spring security

--- src/main/java/br/edu/ifpb/pweb2/eureka/auth/AuthService.java
+++ src/main/java/br/edu/ifpb/pweb2/eureka/auth/AuthService.java
@@ -1,8 +1,5 @@
 package br.edu.ifpb.pweb2.eureka.auth;
 
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,22 +8,4 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 
-  private final AuthenticationManager authManager;
-
-  public Authentication authenticate(AuthRequest authBody) {
-    Authentication auth = UsernamePasswordAuthenticationToken.unauthenticated(authBody.name(), "");
-
-    // here it can throw an error
-    // TODO: implement error page
-    auth = this.authManager.authenticate(auth);
-
-    return auth;
-  }
-
-  public AuthStatus isAuthenticated(Authentication auth) {
-    if (auth == null || !(auth.getPrincipal() instanceof CustomUserDetails))
-      return new AuthStatus(false, null);
-
-    return new AuthStatus(true, ((CustomUserDetails) auth.getPrincipal()).getUsername());
-  }
 }
