4c4eebdf7583994d492818e88859bccd863d5621
Arthur <arthurfelipe5567@gmail.com>
2026-04-20 00:29:47 -0300

    chore(seed): add sample races and question on startup

--- src/main/java/br/edu/ifpb/pweb2/eureka/EurekaApplication.java
+++ src/main/java/br/edu/ifpb/pweb2/eureka/EurekaApplication.java
@@ -1,10 +1,20 @@
 package br.edu.ifpb.pweb2.eureka;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import br.edu.ifpb.pweb2.eureka.question.Question;
+import br.edu.ifpb.pweb2.eureka.question.difficulty.Difficulty;
+import br.edu.ifpb.pweb2.eureka.question.dto.QuestionCreateDto;
+import br.edu.ifpb.pweb2.eureka.race.Race;
+import br.edu.ifpb.pweb2.eureka.race.RaceRepository;
+import br.edu.ifpb.pweb2.eureka.race.RaceService;
+import br.edu.ifpb.pweb2.eureka.race.dto.RaceCreateDto;
 import br.edu.ifpb.pweb2.eureka.user.User;
 import br.edu.ifpb.pweb2.eureka.user.UserRepository;
 
@@ -16,7 +26,7 @@ public class EurekaApplication {
   }
 
   @Bean
-  CommandLineRunner initAdminUser(UserRepository userRepo) {
+  CommandLineRunner initAdminUser(UserRepository userRepo, RaceRepository raceRepo) {
     var name = "nilce";
 
     return args -> {
@@ -28,6 +38,30 @@ public class EurekaApplication {
 
         userRepo.save(admin);
       }
+
+      var race1 = new Race();
+      race1.setTitle("Test1");
+      race1.setDescription("Test1");
+      race1.setDuration(10);
+      race1.setActive(false);
+      raceRepo.save(race1);
+
+      var race2 = new Race();
+      race2.setTitle("Test2");
+      race2.setDescription("Test2");
+      race2.setDuration(10);
+      race2.setActive(true);
+
+      var newQuestion = new Question();
+      newQuestion.setStatement("Question");
+      newQuestion.setDifficulty(Difficulty.HARD);
+      newQuestion.setAnswers(List.of("Answer1", "Answer2"));
+      newQuestion.setCorrectAnswer(1);
+
+      race2.addQuestion(newQuestion);
+
+      raceRepo.save(race2);
+
     };
   }
 
